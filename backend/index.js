16c60400732a1fdbb0b5f580fa65fdfcedb5a7d1
John Doe <johndoe@email.com>
2025-04-07 17:45:36 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
 }))
 
-
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
