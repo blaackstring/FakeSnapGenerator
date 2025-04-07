c6f251ba214da9af7037fb341ae8a1e5b4302151
John Doe <johndoe@email.com>
2025-04-07 19:40:10 +0530

    update indx.js

--- backend/index.js
+++ backend/index.js
@@ -26,9 +26,9 @@ app.use(cors({
 
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
 
 
