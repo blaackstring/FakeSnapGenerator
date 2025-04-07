d460d945162104dd19fbc479ca4b116b52ec9488
John Doe <johndoe@email.com>
2025-04-07 19:28:13 +0530

    updated index.json

--- backend/index.js
+++ backend/index.js
@@ -26,11 +26,11 @@ app.use(cors({
 
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend","dist", "index.html"));
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
+
 
 app.use('/api/auth',Authrouter);
 
