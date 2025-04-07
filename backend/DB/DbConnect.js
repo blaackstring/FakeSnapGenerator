36402808fa50d3c0ef02fea6d710701757b471c8
John Doe <johndoe@email.com>
2025-04-07 17:38:08 +0530

    db conection.js file updated

--- backend/DB/DbConnect.js
+++ backend/DB/DbConnect.js
@@ -5,7 +5,7 @@ console.log(process.env.MONGODB_URL,process.env.MONGODB_NAME);
 
 export const  DbConnect=async()=>{
  try {
-    const connect=mongoose.connect(`${process.env.MONGODB_URL}/${process.env.MONGODB_NAME}`)
+    const connect= await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.MONGODB_NAME}`)
     console.log("MongoDB connected successfully")
     return true;
  } catch (error) {
