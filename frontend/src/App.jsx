0d06d10b28c24b3bde2f5fff79636abebbda68f4
mohd shahan siddiqui <133672067+blaackstring@users.noreply.github.com>
2025-04-10 22:31:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         .then(res => res.json())
         .then(data => {
          console.log(data);
+          console.log("Hello");
          if (data.isAuthenticated) {
           dispatch(UserInfo({ ...data.User }));
           const publicRoutes = ["/resetPassword"];
